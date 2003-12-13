459a37f870ccbf20a1956ad540ff8e4863184a69
Guan Yang <guan@unicast.org>
2003-12-13 18:03:35 +0000

    Upgrade script for lars-blogger from 1.0a3 to 1.0a4

---
+++ sql/postgresql/upgrade/upgrade-1.0a3-1.0a4.sql
@@ -0,0 +1,14 @@
+-- Add a table for ping URLs i.e. services that use the weblogs.com
+-- ping interface.
+create table weblogger_ping_urls (
+    package_id      integer
+                    constraint weblogger_ping_urls_package_id_fk
+                        references apm_packages(package_id),
+    ping_url        varchar(500)
+                    constraint weblogger_ping_urls_ping_url_nn
+                        not null,
+    creation_date   timestamptz
+			default now(),
+    constraint weblogger_ping_urls_pk
+        primary key(package_id, ping_url)
+);
