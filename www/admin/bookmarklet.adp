6f4019350342caf70331526bee4b709085000ebb
Guan Yang <guan@unicast.org>
2003-12-13 19:57:28 +0000

    Added a feature to lars-blogger with a page that will generate a bookmarklet. If a user drags the bookmarklet link to his or her toolbar, the button can be used to instantly blog any page visited.

---
+++ www/admin/bookmarklet.adp
@@ -0,0 +1,13 @@
+<master>
+<property name="title">@blog_name@: Bookmarklet</property>
+<property name="context">@context;noquote@</property>
+
+<p>
+    Drag the bookmarklet link to your Bookmarks/Favorites toolbar.
+    You can then quickly blog about any page you visit by selecting
+    some text that will be quoted and clicking on your bookmarklet.
+</p>
+
+<p>
+    <b><a href="@bookmarklet_link@">Bookmarklet Link</a></b>
+</p>
---
+++ www/admin/bookmarklet.tcl
@@ -0,0 +1,14 @@
+ad_page_contract {
+    Generate a bookmarlet for posting on the blog quickly.
+    
+    @author Guan Yang (guan@unicast.org)
+    @creation-date 2003-12-13
+}
+
+set package_id [ad_conn package_id]
+set entry_edit_url "[ad_url][lars_blog_public_package_url]entry-edit"
+
+set blog_name [lars_blog_name]
+set context [list "Bookmarklet"]
+
+set bookmarklet_link "javascript:d=document;w=window;t='';if(d.selection){t=d.selection.createRange().text}else%20if(d.getSelection){t=d.getSelection()}else%20if(w.getSelection){t=w.getSelection()}void(w.open('$entry_edit_url?title='+escape(d.title)+'&title_url='+escape(d.location.href)+'&content='+escape(t),'_blank','scrollbars=yes,width=500,height=575,status=yes,resizable=yes,scrollbars=yes'))"
