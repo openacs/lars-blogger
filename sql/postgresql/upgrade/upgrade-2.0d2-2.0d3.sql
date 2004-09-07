bbac31214e541c5b340ba26ea5a2111384c5a097
Hector Amado <hr_amado@galileo.edu>
2004-09-07 16:25:51 +0000

    Script did not work for postgreSQL, I changed it. (trigger)

--- sql/postgresql/upgrade/upgrade-2.0d2-2.0d3.sql
+++ sql/postgresql/upgrade/upgrade-2.0d2-2.0d3.sql
@@ -1,5 +1,5 @@
-create function pinds_blog_entries__utrg ()
-returns opaque as '
+create function pinds_blog_entries__utrg()
+returns trigger as '
 begin
     if (new.draft_p = ''t'' and old.draft_p = ''f'') or new.deleted_p = ''t'' then
         perform search_observer__enqueue(old.entry_id,''DELETE'');
@@ -9,6 +9,6 @@ begin
     return old;
 end;' language 'plpgsql';
 
-create or replace trigger pinds_blog_entries__utrg after update on pinds_blog_entries
-for each row execute procedure pinds_blog_entries__utrg ();
-
+create trigger pinds_blog_entries__utrg
+after update on pinds_blog_entries for each row execute
+procedure pinds_blog_entries__utrg();
