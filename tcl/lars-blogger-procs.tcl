ed90e77e6e541d4fe31fa1207cc39f2308f27293
Enrique Catalan <ec@ecatalan.com>
2005-06-13 16:36:12 +0000

    Adding MergePackageUser and MergeShowUserInfo implementations to lars blogger

--- tcl/lars-blogger-procs.tcl
+++ tcl/lars-blogger-procs.tcl
@@ -7,6 +7,47 @@ ad_library {
 
 namespace eval lars_blogger {}
 
+namespace eval lars_blogger::merge {
+
+    ad_proc -callback MergeShowUserInfo -impl lars_blogger {
+	-user_id:required
+    } {
+	Show the items of user_id
+    } {
+	set msg "Lars_blogger items of $user_id"
+	set result [list $msg]
+
+	set channels [db_list_of_lists sel_channels { *SQL* }]
+	lappend result $channels
+	
+	return $result
+    }
+
+    ad_proc -callback MergePackageUser -impl lars_blogger {
+	-from_user_id:required
+	-to_user_id:required
+    } {
+	Merge the entries of two users.
+
+	The from_user_id is the user_id of the user
+	that will be deleted and all the entries
+	of this user will be mapped to the to_user_id.
+	
+    } {
+	set msg "Merging lars_blogger"
+	ns_log Notice $msg
+	set result [list $msg]
+
+	db_dml upd_channels { *SQL* }
+	
+	lappend result "Merge of lars_blogger is done"
+	
+	return $result
+    }
+}
+
+
+
 ad_proc lars_blog_setup_feed {
     -user:boolean
     {-package_id ""}
